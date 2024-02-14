9f9709ae8b652da149efdc782f67a3b033dfd7ee
muskankaur210 <160047204+muskankaur210@users.noreply.github.com>
2024-02-14 23:39:01 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Description: This script uses objects to greet a person and wish them a happy birthday.
+# Define an object called "name" with the value "Isaac"
+name = "Isaac"
+# Define another object called "age" with the value 40
+age = 40
+# Print a greeting and birthday message using the objects
+print(f"How are you {name}? Happy {age}th birthday!")
